86dafd2cb69ae9caa65ddca6ef73cb77e2558f8c
Ajla Halilcevic <ahalilcevic04@gmail.com>
2026-04-10 03:27:26 +0200

    Add RecyclerView movie list to main screen

--- app/src/main/java/com/example/miniimdb/MainActivity.java
+++ app/src/main/java/com/example/miniimdb/MainActivity.java
@@ -2,23 +2,32 @@ package com.example.miniimdb;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.miniimdb.adapter.MovieAdapter;
+import com.example.miniimdb.data.MovieData;
+import com.example.miniimdb.model.Movie;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewMovies;
+    private ArrayList<Movie> movieList;
+    private MovieAdapter movieAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
+
+        movieList = MovieData.getMovies();
+        movieAdapter = new MovieAdapter(movieList);
+
+        recyclerViewMovies.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewMovies.setAdapter(movieAdapter);
     }
 }
---
+++ app/src/main/java/com/example/miniimdb/adapter/MovieAdapter.java
@@ -0,0 +1,55 @@
+package com.example.miniimdb.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.miniimdb.R;
+import com.example.miniimdb.model.Movie;
+
+import java.util.ArrayList;
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private final ArrayList<Movie> movieList;
+
+    public MovieAdapter(ArrayList<Movie> movieList) {
+        this.movieList = movieList;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewTape) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie, parent, false);
+        return new MovieViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie movie = movieList.get(position);
+
+        holder.textMovieTitle.setText(movie.getTitle());
+        holder.textMovieGenre.setText(movie.getGenre());
+        holder.textMovieRating.setText("Rating: " + movie.getRating());
+        holder.imageMoviePoster.setImageResource(movie.getPosterResId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageMoviePoster;
+        TextView textMovieTitle, textMovieGenre, textMovieRating;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageMoviePoster = itemView.findViewById(R.id.imageMoviePoster);
+            textMovieTitle = itemView.findViewById(R.id.textMovieTitle);
+            textMovieGenre = itemView.findViewById(R.id.textMovieGenre);
+            textMovieRating = itemView.findViewById(R.id.textMovieRating);
+        }
+    }
+}
